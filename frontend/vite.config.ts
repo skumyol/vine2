f864f02d0bd4c521c6e93484b86405f31dfc0eec
serkan kumyol <skumyol@hotmail.com>
2026-04-20 21:18:00 +0800

    base url

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: './',
+  base: process.env.VITE_BASE_URL || './',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
